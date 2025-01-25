5ea34f3373ccbe40284e81f1805e3221a0f2792c
Musab Olurode <olurodemusab@gmail.com>
2025-01-25 15:59:16 +0100

    chore: add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	<Analytics />;
+
 	return <Component {...pageProps} />;
 }
 
